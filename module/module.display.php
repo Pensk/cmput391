971c19d0f079d36f69235211fd9517a0ab2d5218
Pensk <pensk@kotobadon.com>
2016-03-29 15:37:11 -0600

    display module

--- module/module.display.php
+++ module/module.display.php
@@ -1,1 +1,34 @@
-//display module
+<?php
+include_once('consants.php');
+//display module for displaying images from the database
+
+Class Display {
+
+  //Holds the database object for interacting with the DB
+  private $db;
+
+  function __construct($db=NULL){
+    //Make a new connection only if we don't have one already.
+    if(is_object($db))
+    {
+        $this->db = $db;
+    } else {
+        $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.';charset=UTF8';
+        $opt = array(
+          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        );
+        $this->db = new PDO($dsn, DB_USER, DB_PASS, $opt);
+    }
+  }
+
+  //return an array of all the images in the database
+  public function displayImages(){
+    $sql = "SELECT * FROM images";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execte();
+    return $stmt->fetchAll();
+  }
+
+}
+?>
--- profile.php
+++ profile.php
@@ -4,6 +4,10 @@
   if(!isset($_SESSION["user"])){
     header("Location: login.php");
   }
+
+  include_once('module/module.display.php');
+  $disp = new Display;
+
 ?>
 
 <html>
@@ -11,5 +15,12 @@
 <h2>Profile Page - <?= $_SESSION["user"] ?></h2>
 <br />
 <a href="upload.php"><h4>Upload an Image</h4></a>
-
+<br />
+<?php
+  foreach($disp->displayImages() as $img):
+?>
+  <?= $img["photo"] ?>
+<?php
+  endforeach;
+?>
 </html>
