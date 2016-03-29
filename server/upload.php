3152bd03c57e510416b4515afe78a0264911beee
Pensk <pensk@kotobadon.com>
2016-03-29 17:05:22 -0600

    upload array

--- server/upload.php
+++ server/upload.php
@@ -4,12 +4,16 @@
   include_once("../module/module.upload.php");
   $upload = new Upload;
 
+  //prepare the variables to be inserted in the database
   $user = $_SESSION["user"];
+  //get the binary data from the image file
   $image = file_get_contents($_FILES["image"]["tmp_name"]);
   $descr = $_FILES["image"]["name"];
 
+  //make sure the user uploaded an image of a valid size!
   if($_FILES["image"]["size"] != 0){
     $upload->uploadImage($user,$descr,$image);
   }
+  //redirect them back to their profile page
   header('Location: ../profile.php');
 ?>
