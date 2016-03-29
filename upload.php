706fddbb653235a0390df4820ebae58b854dd5e2
Pensk <pensk@kotobadon.com>
2016-03-29 17:05:48 -0600

    upload array

--- upload.php
+++ upload.php
@@ -16,7 +16,7 @@
 <h4>Upload .jpg or .gif images to be stored in the database.</h4>
 <form enctype="multipart/form-data" action="server/upload.php" method="POST">
 <input type="hidden" name="MAX_FILE_SIZE" value="64000" />
-Upload (Max 64KB): <input name="image[]" type="file" />
+Upload (Max 64KB): <input name="image[]" type="file" /><input name="image[]" type="file" />
 <input type="submit" value="Upload" />
 </form>
 
