ca09800148c4de12ff62319d4faec54a0374f76d
Pensk <pensk@hotmail.ca>
2016-03-30 14:37:41 -0600

    mult upload test

--- upload.php
+++ upload.php
@@ -16,7 +16,7 @@
 <h4>Upload .jpg or .gif images to be stored in the database.</h4>
 <form enctype="multipart/form-data" action="server/upload.php" method="POST">
 <input type="hidden" name="MAX_FILE_SIZE" value="64000" />
-Upload (Max 64KB): <input name="image" webkitdirectory directory multiple type="file" /><br />
+Upload (Max 64KB): <input name="image[]" webkitdirectory directory multiple type="file" /><br />
 <input type="radio" name="security" value="private" checked />Private
 <input type="radio" name="security" value="public" />Public<br />
 <input type="text" name="description" placeholder="Description" /><br />
