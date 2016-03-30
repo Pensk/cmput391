ab3fef09b65c445c1f6620b07f29f8ea341ffb1a
Pensk <pensk@hotmail.ca>
2016-03-30 14:32:10 -0600

    upload form

--- upload.php
+++ upload.php
@@ -17,6 +17,12 @@
 <form enctype="multipart/form-data" action="server/upload.php" method="POST">
 <input type="hidden" name="MAX_FILE_SIZE" value="64000" />
 Upload (Max 64KB): <input name="image" webkitdirectory directory multiple type="file" />
+<input type="text" name="description" placeholder="Description" /><br />
+<input type="radio" name="security" value="private" checked />Private
+<input type="radio" name="security" value="public" />Public<br />
+<input type="text" name="location" placeholder="Location" /><br />
+<input type="text" name="time" placeholder="Time" /><br />
+<input type="text" name="subject" placeholder="Subject" /><br />
 <input type="submit" value="Upload" />
 </form>
 
