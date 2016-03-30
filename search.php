dbcf9a6e091e982121e065f106f9f525cc9b595b
MikeZzs <zishen@ualberta.ca>
2016-03-29 21:50:08 -0600

    Update search.php

--- search.php
+++ search.php
@@ -1,5 +1,27 @@
+<?php
+// if logged in...
+  session_start();
+  if(isset($_POST["submit"])){
+    $searchtext = $_POST["Searchtext"];
+    $thelist = explode(' ',$searchtext);
+    $keys = count($thelist);
+    //$query='select * from [img_data_table] where [img_data_table].name like $thelist(1).....
+    //
+    //
+    //link to ora module
+  }else{
+    // to search page
+  }
+?>
 <html>
 
-Search Page
-
+<head>
+  <title>Search Page</title>
+</head>
+<body>
+<form method="POST">
+    <input type="text" name="Searchtext" placeholder="Searchtext">
+    <input type="submit" name="submit">
+  </form>
+  </body>
 </html>
