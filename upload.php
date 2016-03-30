659d60dddbf09ddf51de17b55c2dd2f0ee6b5fb6
Pensk <pensk@hotmail.ca>
2016-03-30 15:32:56 -0600

    uplaod form layout

--- upload.php
+++ upload.php
@@ -10,22 +10,29 @@
 include_once('template/header.php');
 ?>
 <body>
+<div class="container">
 <h3>Upload Page</h3>
 
 <h4>Upload .jpg or .gif images to be stored in the database.</h4>
 <form enctype="multipart/form-data" action="server/upload.php" method="POST">
-<input type="hidden" name="MAX_FILE_SIZE" value="64000" />
-Upload (Max 64KB): <input name="image[]" webkitdirectory directory multiple type="file" /><br />
-<input type="radio" name="permitted" value="false" checked />Private
-<input type="radio" name="permitted" value="true" />Public
-<input type="radio" name="permitted" value="group"/>Or Group ID: <input type="text" name="group" placeholder="group ID" /><br />
-<input type="text" name="description" placeholder="Description" /><br />
-<input type="text" name="location" placeholder="Location" /><br />
-<input type="date" name="time" placeholder="Date (Y-M-D)" /><br />
-<input type="text" name="subject" placeholder="Subject" /><br />
-<input type="submit" value="Upload" />
+  <div class="form-group"<
+<input class="form-control" type="hidden" name="MAX_FILE_SIZE" value="64000" />
+Upload (Max 64KB): <input class="form-control" name="image[]" webkitdirectory directory multiple type="file" /><br />
+</div>
+<div class="form-group">
+<input class="form-control" type="radio" name="permitted" value="false" checked />Private
+<input class="form-control" type="radio" name="permitted" value="true" />Public
+<input class="form-control" type="radio" name="permitted" value="group"/>Or Group ID: <input class="form-control" type="text" name="group" placeholder="group ID" /><br />
+</div>
+<div class="form-group"<
+<input class="form-control" type="text" name="description" placeholder="Description" /><br />
+<input class="form-control" type="text" name="location" placeholder="Location" /><br />
+<input class="form-control" type="date" name="time" placeholder="Date (Y-M-D)" /><br />
+<input class="form-control" type="text" name="subject" placeholder="Subject" /><br />
+<input class="form-control" type="submit" value="Upload" />
+</div>
 </form>
-
+</div>
 </body>
 <?php
 include_once('template/footer.php');
