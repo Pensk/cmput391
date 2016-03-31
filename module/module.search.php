7334af617b6e9b10c4ce5c023251631ea83a7016
Pensk <pensk@hotmail.ca>
2016-03-31 16:12:09 -0600

    search module test

--- module/module.search.php
+++ module/module.search.php
@@ -19,13 +19,20 @@ Class Search {
         );
         $this->db = new PDO($dsn, DB_USER, DB_PASS, $opt);
     }
-  } 
+  }
   function search($thelist=array(),$keys=0,$startdate="",$enddate=""){
+    //Test for now - let's start by returning all images in the database
+    $sql = "SELECT * FROM images";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll();
+    
+    /*
     $templist="";$tempstartdate="";$tempenddate="";
     if($keys!=0){
-      for($temp=0;$temp<$keys;%temp++){
+      for($temp=0;$temp<$keys;$temp++){
         if($temp==0){
-          $templist=" (imgaes.subject like ".$thelist[0]." or imgaes.place like ".$thelist[0]." or images.description like ".$thelist[0].")";
+          $templist=" (images.subject like ".$thelist[0]." or imgaes.place like ".$thelist[0]." or images.description like ".$thelist[0].")";
         }
         else{
           $templist=$templist." and (images.(namevalue) like ".$thelist[$temp]." or images.place like ".$thelist[$temp]." or images.description like ".$thelist[$temp].")";
@@ -51,5 +58,7 @@ Class Search {
       $sql="select * from images where".$templist.$tempstartdate.$tempenddate;
       //execute
     }
+    */
   }
+}
 ?>
