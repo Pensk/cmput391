1c629a948f7a4a5983c096a2ac0e76729b002d7d
Pensk <pensk@hotmail.ca>
2016-03-31 20:37:04 -0600

    image viewing security hole

--- server/image.php
+++ server/image.php
@@ -1,10 +1,14 @@
 <?php
+  session_start();
+  if(!isset($_SESSION["user"]))
+    header('Location: ../login.php');
   include_once('../module/module.display.php');
   $disp = new Display;
 
   $imgid = $_GET["id"];
 
-  $img = $disp->getImageFromId($imgid);
+  if($disp->canView($_SESSION["user"],$imgid))
+    $img = $disp->getImageFromId($imgid);
 
   header('Content-type: image/jpeg');
 
