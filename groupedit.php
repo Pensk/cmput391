b5b3129db45dfe3f16aa7906f65d663a47f35c6f
Pensk <pensk@hotmail.ca>
2016-03-31 19:26:42 -0600

    group edit

--- groupedit.php
+++ groupedit.php
@@ -21,9 +21,21 @@
     <h3>Error - You don't have permission to edit this group.</h3>
     <?php
       else:
+      $groupinfo = $group->getInfo($groupid);
     ?>
-    <h2>Edit Group</h2>
-
+    <h2>Edit Group</h2><br />
+    <h4>Name</h4>
+    <form class="form" action="server/groupname.php" method="POST">
+      <input type="text" value="<?= $groupinfo["group_name"] ?>" name="groupname" />
+      <input type="submit" name="submit" value="Update Name" />
+    </form>
+    <ul>
+      <?php
+        foreach($group->getMembers($groupid) as $member){
+          echo "<li>".$member['user_name']."<span class='badge'><a href='server/groupdeleteuser?id=".$member['user_name']."'>X</a></span></li>";
+        }
+      ?>
+    </ul>
     <?php
       endif;
     ?>
