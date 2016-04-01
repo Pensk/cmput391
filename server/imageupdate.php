914b5f0056a1d958b662334ea3108e62c0dc821b
Pensk <pensk@hotmail.ca>
2016-03-31 20:52:06 -0600

    image updating

---
+++ server/imageupdate.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+  if(!isset($_SESSION["user"]))
+    header('Location: ../login.php');
+  include_once('../module/module.display.php');
+  $disp = new Display;
+
+  $imgid = $_POST["id"];
+
+  if($disp->canEdit($_SESSION["user"],$imgid)) {
+    $descr = $_POST["description"];
+    $loc = $_POST["place"];
+    $time = date('Y-m-d',strtotime($_POST["timing"]));
+    $subj = $_POST["subject"];
+    $img = $disp->updateImage($imgid,$descr,$loc,$time,$subj);
+  }
+
+  header('Content-type: image/jpeg');
+
+  echo $img;
+?>
