ed9fe0a6ae29c15a49510cbfe806623ae2948d0c
Pensk <pensk@hotmail.ca>
2016-04-01 15:16:04 -0600

    database class

---
+++ module/module.database.php
@@ -0,0 +1,16 @@
+<?php
+  include_once('constants.php');
+
+  class Database{
+
+    public $pdo;
+
+    function __construct(){
+      $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.';charset=UTF8';
+      $opt = array(
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      );
+      $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $opt);
+    }
+  }
+?>
