e52df35146b90a959192b7e1cd30c6845da8102d
Pensk <pensk@hotmail.ca>
2016-03-31 20:51:59 -0600

    image updating

--- module/module.display.php
+++ module/module.display.php
@@ -82,5 +82,11 @@ Class Display {
     return $stmt->fetch();
   }
 
+  public function updateImage($imgid,$descr,$loc,$time,$subj) {
+    $sql = "UPDATE images SET description = :descr, place = :loc, timing = :timing, subject = :subj WHERE photo_id = :imgid";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute(["descr"=>$descr,"loc"=>$loc,"timing"=>$time,"subj"=>$subj,"imgid"=>$imgid]);
+  }
+
 }
 ?>
