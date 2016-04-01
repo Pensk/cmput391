0859e34362737d98da871675c489b7257888bed8
Pensk <pensk@hotmail.ca>
2016-04-01 09:34:27 -0600

    search thumbs

--- search.php
+++ search.php
@@ -40,7 +40,7 @@
     $c = 0;
     foreach($result as $img):
       if($c % 3 == 0)
-        echo "</div>\n<div class='clearfix'></div>\n<div class='row'>";
+        echo "</div>\n<div class='clearfix'></div><br />\n<div class='row'>";
   ?>
   <div class="col-md-4 text-center">
   <img src="server/image.php?id=<?= $img["photo_id"] ?>" class="center-block" width="100" height="100"/><br />
