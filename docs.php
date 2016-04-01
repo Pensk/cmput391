61716e555d2acd425a3d6129efd9a402175e46f8
Pensk <pensk@hotmail.ca>
2016-04-01 17:50:27 -0600

    docs

---
+++ docs.php
@@ -0,0 +1,74 @@
+<?php
+  include_once('template/header.php');
+?>
+<div class="container">
+Documentation:
+<p>Online Image Sharing System Documentation</p>
+
+<p>System Installation: </p>
+
+<p>Client side: Any modern browser will do.</p>
+
+<p>Server side:PHP5 required. Server installation:&lt;copy files in …to….EDIT&gt; </p>
+
+<p>Database side: Any database server.</p>
+
+<p>User manual:</p>
+
+<p>Modules: </p>
+
+<p>1)Profile </p>
+
+<p>2)Upload </p>
+
+<p>3)Search </p>
+
+<p>4)Groups management </p>
+
+<p>5)Logout</p>
+
+<p>All modules are accessible on website’s header.</p>
+
+<p>All modules require a successful login to access.</p>
+
+<p>New user registration is available on index page and login page.</p>
+
+<p>Profile:</p>
+
+<p>· Shows your uploaded images</p>
+
+<p>· Shows most popular images on the server</p>
+
+<p>Upload page:</p>
+
+<p>· Choose your image files and upload them suing &lt;Choose Files&gt;</p>
+
+<p>· Setup your images properties below </p>
+
+<p>Search:</p>
+
+<p>· Keywords search and date search available</p>
+
+<p>· Can be active by both methods.</p>
+
+<p>· Keyword search searches images that has all keywords mentioned.</p>
+
+<p>· Time search searches image with their time interval lay within the given period.</p>
+
+<p>Groups:</p>
+
+<p>· Shows which groups you own.</p>
+
+<p>· Show which groups you are in.</p>
+
+<p>· Can create a group.</p>
+
+<p>· Can edit your existing groups.</p>
+
+<p>Logout: </p>
+
+<p>· Clear up all resources and log you out. Returning you to the login page.</p>
+</div>
+<?php
+  include_once('template/footer.php');
+?>
